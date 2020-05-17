6c210fb9d0208445fee73ae190ebf572d0bf0ec8
diego <diego@diego-ASUSLaptop>
2020-05-17 17:11:02 +0200

    proxy update

--- proxy/resource/src/main/java/demo/ResourceApplication.java
+++ proxy/resource/src/main/java/demo/ResourceApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableRedisHttpSession
 public class ResourceApplication extends WebSecurityConfigurerAdapter {
 
 	@RequestMapping("/")
