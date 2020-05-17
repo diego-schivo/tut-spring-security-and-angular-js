6cee6ed887fccd3d8e17b1f65ad8f6f807b23a6f
diego <diego@diego-ASUSLaptop>
2020-05-17 10:37:52 +0200

    basic update

--- basic/src/main/java/demo/UiApplication.java
+++ basic/src/main/java/demo/UiApplication.java
@@ -6,6 +6,14 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +32,33 @@ public class UiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
 	}
+
+    @Configuration
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    	@Override
+    	protected void configure(HttpSecurity http) throws Exception {
+    		// @formatter:off
+    		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+			.httpBasic();
+    		// @formatter:on
+    	}
+
+        @Bean
+    	@Override
+    	public UserDetailsService userDetailsService() {
+    		UserDetails user =
+    			 User.withDefaultPasswordEncoder()
+    				.username("user")
+    				.password("password")
+    				.roles("USER")
+    				.build();
+
+    		return new InMemoryUserDetailsManager(user);
+    	}
+    }
 
 }
