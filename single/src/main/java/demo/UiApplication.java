a9cbd3b33f1abeb823a179fb687a599baa1b8161
diego <diego@diego-ASUSLaptop>
2020-05-17 11:17:58 +0200

    single update

--- single/src/main/java/demo/UiApplication.java
+++ single/src/main/java/demo/UiApplication.java
@@ -7,11 +7,14 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,7 +49,6 @@ public class UiApplication {
     }
 
     @Configuration
-    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
     protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
@@ -54,13 +56,26 @@ public class UiApplication {
             http
                 .httpBasic().and()
                 .authorizeRequests()
-                    .antMatchers("/index.html", "/", "/home", "/login").permitAll()
+                    .antMatchers("/index.html", "/", "/home", "/login", "/*.js", "/favicon.ico").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .csrf()
                     .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
             // @formatter:on
         }
+
+        @Bean
+    	@Override
+    	public UserDetailsService userDetailsService() {
+    		UserDetails user =
+    			 User.withDefaultPasswordEncoder()
+    				.username("user")
+    				.password("password")
+    				.roles("USER")
+    				.build();
+
+    		return new InMemoryUserDetailsManager(user);
+    	}
     }
 
 }
